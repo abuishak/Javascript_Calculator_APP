802188925f8f523b557ef4809f1c12040b176192
abuishak <39011865+abuishak@users.noreply.github.com>
2020-08-06 20:51:13 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+//var resultField = $("#result")
+
+function insertNumber(number){
+var existingNumbers = $("#result").val()
+   $("#result").val(existingNumbers + number)
+}
+
+function clearResult(){
+   $("#result").val('')
+}
+function calculate(){
+ var result= eval($("#result").val())  
+ $("#result").val(result)
+}
+function deletNumber(){
+   var presentValue= $("#result").val()
+   if (presentValue!=''){
+      $("#result").val(presentValue.slice(0,-1));
+   }
+   
+  }
